0e8fbb37eb2bb98e8b38ed42141c1711a352946d
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2011-09-03 18:26:13 +0200

    Add some F# experiments

---
+++ FSharpTest/CutBoolExperiments/CutBoolExperiments/Program.fs
@@ -0,0 +1,26 @@
+﻿// Learn more about F# at http://fsharp.net
+
+
+let testNeighborHoodsA = 
+    Array.ofSeq [ 
+        Set.ofSeq [ 1; 2; 5 ]
+        Set.ofSeq [ 1; 2; 3; ]
+        Set.ofSeq [ 4; ]
+        Set.ofSeq [ 5; ]
+        ]
+
+
+let hoods = testNeighborHoodsA
+
+let results = 
+    testNeighborHoodsA |>
+    Seq.map (fun x -> Set.union x testNeighborHoodsA.[0]) |>
+    Set.ofSeq
+
+Seq.iter (
+        fun x -> (
+                 printfn "%s" (
+                    String.concat " " (
+                        Seq.map (fun y -> y.ToString()) x))))
+    results // testNeighborHoodsA.[0]
+
