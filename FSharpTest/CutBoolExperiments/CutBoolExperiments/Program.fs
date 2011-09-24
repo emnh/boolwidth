3bcc1162ff3ff5fdf3e61c0721fa9161a371a709
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2011-09-24 23:23:11 +0200

    Implement new cutbool algo. Started implementing connected components optimization.

--- FSharpTest/CutBoolExperiments/CutBoolExperiments/Program.fs
+++ FSharpTest/CutBoolExperiments/CutBoolExperiments/Program.fs
@@ -2,6 +2,25 @@
 
 namespace Experiments
 
+module Graph =
+
+    type IGraph<'a> = 
+        abstract member getNodes : unit -> seq<'a>
+        abstract member getNeighbors : 'a -> seq<'a>
+
+    type Node<'a> = {
+            Content : 'a;
+            mutable Neighbors : Node<'a>[];
+            mutable IsProcessed : bool;
+    }
+
+    let rec dfs (graph : IGraph<'a>) getNeighbors node seen =
+        seq {
+            yield node
+            for neighbor in (getNeighbors node) do
+                yield! dfs graph neighbor
+        }
+
 module ListNeighborHoods =
     let private internalListNeighborHoods hoods newHood = 
         (Set.union
@@ -16,12 +35,91 @@ module ListNeighborHoods =
 //let results = testNeighborHoodsA
 //let results2 = internalListNeighborHoods testNeighborHoodsA testNeighborHoodsA.[0]
 
-    let countNeighborHoods neighborHoods = 
+    let formatSet set = (String.concat " " (Seq.map (fun y -> y.ToString()) set))
+
+    let formatTuple tuple =
+        match tuple with 
+            | (msg, inSet, outSet, set) ->
+                sprintf "%s: in(%s) out(%s) set(%s)" msg (formatSet inSet) (formatSet outSet) (formatSet set)
+
+    type BranchResult<'a> =
+    | Count of int
+    | HoodSeq of seq<'a>
+
+    exception TypeMixBug of string
+
+    //let a = if true then Count(2) else HoodSeq(seq { yield 3 })
+
+    let connectedComponents neighborHoods = 
+        let ar = Array.ofSeq neighborHoods
+        let graph = Array.ofSeq seq {
+            for h1 in neighborHoods do
+                yield {
+                    Content = h1; 
+                    Neighbors = Array.ofSeq seq {
+                        for h2 in neighborHoods do
+                            if Set.intersect h1 h2 then yield h2
+                    };
+                    IsProcessed = False;
+                }
+        }
+        graph
+        //for (node, neighbors) in graph do    
+
+    let countNeighborHoods neighborHoods countOnly = 
+        let neighborHoods = Set.ofSeq neighborHoods
         let allVertices = (Set.unionMany neighborHoods)
-        let branch inSet outSet rest restNeighborHoods = 
-            let restNeighborhoods = Seq.map (fun x -> Set.intersect x rest) restNeighborHoods
-            2
-        branch Set.empty Set.empty allVertices neighborHoods
+        let wrapSeq = (if countOnly 
+                        then fun aseq -> Count(Seq.length aseq)
+                        else fun aseq -> HoodSeq(aseq))
+        let rec branch inSet outSet rest restNeighborHoods = 
+            //let restNeighborHoods = Set.map (fun x -> Set.intersect x rest) restNeighborHoods
+            
+            if (Set.isEmpty rest) then
+                wrapSeq (seq [ ("fixed", inSet, outSet, inSet) ])
+            elif (Set.isEmpty restNeighborHoods) then 
+                wrapSeq Seq.empty
+            elif (Set.count restNeighborHoods = 1) then
+                wrapSeq (seq [ ("rest", inSet, outSet, Seq.head restNeighborHoods) ])
+            else
+                let selected = Seq.head rest
+                //let containsInSet = (Set.filter (fun neighborHood -> Set.isSuperset neighborHood inSet) restNeighborHoods)
+                let isValidIn = Set.isSubset (Set.add selected inSet) (Set.unionMany restNeighborHoods)
+                let notContainsSelected = (Set.filter (fun neighborHood -> not (Set.contains selected neighborHood)) restNeighborHoods)
+                let isValidOut = Set.isSubset inSet (Set.unionMany notContainsSelected)
+                let inResult = 
+                    (if isValidIn
+                        then (branch 
+                                (Set.add selected inSet)
+                                outSet 
+                                (Set.remove selected rest)
+                                restNeighborHoods)
+                        else wrapSeq Seq.empty)
+
+                let outResult =
+                    (if ((not (Set.isEmpty notContainsSelected)) && isValidOut)
+                        then (branch 
+                                inSet 
+                                (Set.add selected outSet)
+                                (Set.remove selected rest)
+                                notContainsSelected)
+                        else
+                            wrapSeq Seq.empty)
+                match (inResult, outResult) with
+                    | Count(inLen), Count(outLen) -> Count(inLen + outLen)
+                    | HoodSeq(inSeq), HoodSeq(outSeq) -> HoodSeq(seq { 
+                                                            yield! inSeq
+                                                            yield! outSeq
+                                                        })
+                    | _ -> raise (TypeMixBug("Cannot mix Count and HoodSeq"))
+
+        let result = (branch Set.empty Set.empty allVertices neighborHoods) 
+        match result with
+            | Count(len) -> Count(len + 1)
+            | HoodSeq(hseq) -> HoodSeq(seq {
+                yield ("empty", Set.empty, Set.empty, Set.empty)
+                yield! hseq
+            })
 
 
 module Test =
@@ -35,18 +133,22 @@ module Test =
                 [ 5; ]
             ]
 
-    
+    let printResults results =     
+        (Seq.iter (fun x -> (printfn "%s" x)) results)
+
     let createTestNeighborHoods memberCount = 
         Seq.map 
             (fun x -> Set.ofSeq [ x; ])
             [1..memberCount]
+
+    let testNeighborHoodsB = createTestNeighborHoods 16
 
     let results = ListNeighborHoods.listNeighborHoods testNeighborHoodsA
 
     let testCount power =
         let testNeighborHoods = createTestNeighborHoods power
         let count = 
-            Seq.length 
+            Seq.length
                 (ListNeighborHoods.listNeighborHoods testNeighborHoods)
         assert (count = (1 <<< power))
         count
@@ -56,13 +158,21 @@ module Test =
 //                ])
 //            )
 
-    let printResults results =     
-        Seq.iter (
-            fun x -> (
-                     printfn "%s" (
-                        String.concat " " (
-                            Seq.map (fun y -> y.ToString()) x))))
-            results
+    exception Bug of string
+    let testCount2 power =
+        let testNeighborHoods = createTestNeighborHoods power
+        let count = match (ListNeighborHoods.countNeighborHoods testNeighborHoods true) with
+                    | ListNeighborHoods.Count(len) -> len
+                    | _ -> (raise (Bug ""))
+        assert (count = (1 <<< power))
+        count
+
+    let y = ListNeighborHoods.countNeighborHoods testNeighborHoodsB true
+    match y with
+        | ListNeighborHoods.Count(len) -> printfn "y: %d" len
+        | ListNeighborHoods.HoodSeq(y) -> printResults (Seq.map ListNeighborHoods.formatTuple y)
+    
+    //printResults (Seq.map ListNeighborHoods.formatSet results)
 
     let run() =
         [
@@ -90,6 +200,7 @@ module Performance =
         printfn "%s: Needed %f ms" msg (timeit f)
         
     showTiming (fun() -> (Test.testCount 16)) "TestCount"
+    showTiming (fun() -> (Test.testCount2 16)) "TestCount2"
 
     //printfn "%d" (Test.testCount 8)
 
