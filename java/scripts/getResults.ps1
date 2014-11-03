b07ab58dc8fea205870d799835c6ad0ccd2fe0a1
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2014-11-03 15:51:56 +0100

    add getResults script

---
+++ java/scripts/getResults.ps1
@@ -0,0 +1,30 @@
+$out = "out-logs"
+$lower = 271
+$upper = 550
+$currentPath = (Resolve-Path .)
+$graphs = (gci -recurse .\data\graphLib\* -include *.dgf, *.dimacs |
+  select-string "p edges? ([0-9]+) ([0-9]+)" | 
+  % {
+    new-object PSObject -Property @{
+      "FileName"=(Resolve-Path -Relative $_.Path);
+      "Nodes"=[int]$_.matches.groups[1].Value;
+      "Edges"=[int]$_.matches.groups[2].Value 
+    } 
+  } |
+  sort -Desc Nodes | 
+  where { $_.Nodes -ge $lower -and $_.Nodes -lt $upper })
+
+$graphs | % {
+  $FileName = $_.FileName
+  $LogName = (Join-Path $out $FileName) + ".txt"
+  if (!(Test-Path $LogName)) {
+    Write-Output "not done: $FileName"
+  } else {
+    $timeline = gc $LogName | select-string '{ "time.*"BW":'
+    if ($timeline.length -eq 0) {
+      #Write-Output "failed graph: $FileName"
+    } else {
+      Write-Output $timeline | % { $_ -replace '\\','/' }
+    }
+  }
+}
